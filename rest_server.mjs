22e701b86d556e4602f4ad2421283ec917f6cc04
Khalid <moha7680@stthomas.edu>
2023-11-27 13:54:00 -0600

    testing

--- rest_server.mjs
+++ rest_server.mjs
@@ -104,9 +104,27 @@ app.get('/incidents', (req, res) => {
 
 // PUT request handler for new crime incident
 app.put('/new-incident', (req, res) => {
-    console.log(req.body); // uploaded data
-    
-    res.status(200).type('txt').send('OK'); // <-- you may need to change this
+    const { case_number, date, time, code, incident, police_grid, neighborhood_number, block } = req.body;
+
+    const checkIfExistsQuery = 'SELECT * FROM Incidents WHERE case_number = ?';
+    dbSelect(checkIfExistsQuery, [case_number])
+        .then((rows) => {
+            if (rows.length > 0) {
+                res.status(500).type('txt').send('Case number already exists in the database.');
+            } else {
+                const insertQuery = 'INSERT INTO Incidents (case_number, date, time, code, incident, police_grid, neighborhood_number, block) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+                dbRun(insertQuery, [case_number, date, time, code, incident, police_grid, neighborhood_number, block])
+                    .then(() => {
+                        res.status(200).type('txt').send('Incident data inserted successfully.');
+                    })
+                    .catch((error) => {
+                        res.status(500).type('txt').send(error);
+                    });
+            }
+        })
+        .catch((error) => {
+            res.status(500).type('txt').send(error);
+        });
 });
 
 // DELETE request handler for removing a crime incident
